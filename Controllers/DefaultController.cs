909ebc4ad0c060c420b3130711eb23fb6b653ce4
meryem <116671202+meryemorc@users.noreply.github.com>
2025-01-06 03:28:17 +0300

    Add project files.

---
+++ Controllers/DefaultController.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcFirmaCagri.Models.Entity;
+
+namespace MvcFirmaCagri.Controllers
+{
+    [Authorize]
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        DbisTakipEntities db = new DbisTakipEntities();
+        
+        public ActionResult AktifCagrilar()
+        {
+            var cagrilar = db.TblCagrilar.Where(x => x.Durum == true && x.CagrıFirma == 4 ).ToList();
+            return View(cagrilar);
+        }
+        public ActionResult PasifCagrilar()
+        {
+            var cagrilar = db.TblCagrilar.Where(x => x.Durum == false && x.CagrıFirma == 4).ToList();
+            return View(cagrilar);
+        }
+        [HttpGet]
+        public ActionResult YeniCagri()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult YeniCagri(TblCagrilar p)
+        {
+            p.Durum = true;
+            p.Tarih = DateTime.Parse(DateTime.Now.ToShortDateString());
+            p.CagrıFirma = 4;
+            db.TblCagrilar.Add(p);
+            db.SaveChanges();
+            return RedirectToAction("AktifCagrilar");
+        }
+        public ActionResult CagriDetay(int id)
+        {
+            var cagri = db.TblCagriDetay.Where(x => x.Cagri == id).ToList();
+            return View(cagri);
+        }
+        public ActionResult CagriGetir(int id)
+        {
+            var cagri = db.TblCagrilar.Find(id);
+            return View("CagriGetir", cagri);
+        }
+        public ActionResult CagriDuzenle(TblCagrilar p)
+        {
+            var cagri = db.TblCagrilar.Find(p.ID);
+            cagri.Konu = p.Konu;
+            cagri.Aciklama = p.Aciklama;
+            db.SaveChanges();
+            return RedirectToAction("AktifCagrilar");
+        }
+    }
+}
